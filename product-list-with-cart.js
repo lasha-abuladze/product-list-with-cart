5415e801900450fa126bc2550b5fcd32a4e044be
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2024-07-25 00:09:56 +0400

    in javascript file add click event listener on the add to cart button

--- product-list-with-cart.js
+++ product-list-with-cart.js
@@ -5,10 +5,12 @@
 
 
 const productList = document.querySelector(`.product-list`);
-
+const ordersHTML = document.querySelector(`.orders`);
+const emptyCartHTML = document.querySelector(`.empty-cart`);
+const totalOrderNumberHTML = document.querySelector(`.total-order-number`)
 
 productList.textContent = ``;
-
+ordersHTML.textContent = ``;
 
 
 
@@ -46,15 +48,15 @@ const displayProduct = function(data) {
 
                             <button class="btn add-remove-from-cart display-none">
 
-                                <span class="decrement-increment">
+                                <span class="decrement-increment decrement">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" viewBox="0 0 10 2"><path d="M0 .375h10v1.25H0V.375Z"/></svg>
                                 </span>
 
                                 <span class="ordered-product-number">
-                                    1
+                                    0
                                 </span>
 
-                                <span class="decrement-increment">
+                                <span class="decrement-increment increment">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10"><path d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
                                 </span>
 
@@ -103,13 +105,130 @@ const addToCart = function(buttons, data) {
         const addToCartHTML = el.querySelector(`.add-to-cart`);
         const addRemoveFromCartHTML = el.querySelector(`.add-remove-from-cart`);
 
+        const increment = addRemoveFromCartHTML.querySelector(`.increment`)
+        const decrement = addRemoveFromCartHTML.querySelector(`.decrement`)
+        const orderProductNumberHTML = addRemoveFromCartHTML.querySelector(`.ordered-product-number`)
+
+
 
         addToCartHTML.addEventListener(`click`, () => {
-            data[i].order = +1;
+
+
+            // const increment = addRemoveFromCartHTML.querySelector(`.increment`)
+            // const decrement = addRemoveFromCartHTML.querySelector(`.decrement`)
+            // const orderProductNumberHTML = addRemoveFromCartHTML.querySelector(`.ordered-product-number`)
+
+
+
+            data[i].order = data[i].order +1;
+            orderProductNumberHTML.textContent = `${data[i].order}`
+
+            if(data.some(el => el.order > 0)) {
+                emptyCartHTML.classList.add(`display-none`);
+                ordersHTML.classList.remove(`display-none`);              
+
+            }
+
+            totalOrderNumberHTML.textContent = `${data.reduce((acc, el) => acc + el.order, 0)}`
+
 
             addToCartHTML.classList.add(`display-none`);
-            addRemoveFromCartHTML.classList.remove(`display-none`);            
+            addRemoveFromCartHTML.classList.remove(`display-none`); 
+            
+            const orderBox = `
+                <div class="order-box ${data[i].category}">
+
+                    <div class="order-details">
+
+                        <h6>
+                            ${data[i].name}
+                        </h6>
+
+                        <span class="single-product-order-number">
+                            ${data[i].order}x
+                        </span>
+
+                        <span class="single-product-price">
+                            @ $${data[i].price}
+                        </span>
+
+                        <span class="sinlge-product-total-ptice">
+                            $${data[i].price * data[i].order}
+                        </span>
+
+                    </div>
+
+                    <button class="remove-product">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/></svg>
+                    </button>
+
+                </div>
+            `
+
+            ordersHTML.insertAdjacentHTML(`beforeend`, orderBox);
+
+
+            
+            // increment.addEventListener(`click`, () => {
+            //     data[i].order = data[i].order +1;
+            //     totalOrderNumberHTML.textContent = `${data.reduce((acc, el) => acc + el.order, 0)}`
+            //     orderProductNumberHTML.textContent = `${data[i].order}`
+
+
+
+            // })
+
+            // decrement.addEventListener(`click`, () => {
+
+            //     if(data[i].order > 0) {
+            //         data[i].order = data[i].order - 1;
+            //         totalOrderNumberHTML.textContent = `${data.reduce((acc, el) => acc + el.order, 0)}`
+            //         orderProductNumberHTML.textContent = `${data[i].order}`
+            //     } else if (data[i].order = 0) {
+            //         addToCartHTML.classList.remove(`display-none`);
+            //         addRemoveFromCartHTML.classList.add(`display-none`); 
+            //     }
+
+            //     // if(data[i].order = 0) {
+            //     //     // emptyCartHTML.classList.remove(`display-none`);
+            //     //     // ordersHTML.classList.add(`display-none`);
+
+            //     //     console.log(data[i])
+            //     // }
+
+
+            // })
+
         })
+
+
+
+        increment.addEventListener(`click`, () => {
+            data[i].order = data[i].order +1;
+            totalOrderNumberHTML.textContent = `${data.reduce((acc, el) => acc + el.order, 0)}`
+            orderProductNumberHTML.textContent = `${data[i].order}`
+
+
+
+        })
+
+        decrement.addEventListener(`click`, () => {
+
+            if(data[i].order >= 1) {
+                data[i].order = data[i].order - 1;
+                totalOrderNumberHTML.textContent = `${data.reduce((acc, el) => acc + el.order, 0)}`
+                orderProductNumberHTML.textContent = `${data[i].order}`
+            }
+
+
+            if(data[i].order < 1) {               
+            addToCartHTML.classList.remove(`display-none`);
+            addRemoveFromCartHTML.classList.add(`display-none`); 
+            }
+
+
+        })
+
     })
 }
 
